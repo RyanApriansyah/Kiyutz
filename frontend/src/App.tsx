7fdc1684c065ab6b4c04575a5bd329e1131c0e7d
ptraanwarfdlh <menggarongx@gmail.com>
2026-06-21 01:38:14 +0700

    push Putra

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,9 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>OBH Combi Sachet Ekhem Ekhem</h1>
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.tsx</code> and save to test <code>OBH</code>
           </p>
         </div>
         <button
