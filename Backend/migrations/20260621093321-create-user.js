31fc76f63abd33121b6b761ba3ec79cea37a068e
ptraanwarfdlh <menggarongx@gmail.com>
2026-06-21 16:58:00 +0700

    setup sequelize

---
+++ Backend/migrations/20260621093321-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      idUser: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nama: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      statusUser: {
+        type: Sequelize.ENUM('Active', 'Inactive'),
+        defaultValue: 'Active'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ Backend/models/user.js
@@ -0,0 +1,54 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    nama: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+       notNull: { msg: 'Nama harus diisi' },
+       notEmpty: { msg: 'Nama harus diisi' },
+     },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+       notNull: { msg: 'Password harus diisi' },
+       notEmpty: { msg: 'Password harus diisi' },
+     },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg : 'Email sudah digunakan'
+      },
+      validate: {
+       isEmail: {msg: 'Format yang digunakan buakn format email'},
+       notNull: { msg: 'Email harus diisi' },
+       notEmpty: { msg: 'Email harus diisi' },
+     },
+    },
+    statusUser: {
+      type: DataTypes.ENUM,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
